21e833ffe5e677efeb3f29454a5eb7568f833549
Istar <Istar@192.168.0.16>
2017-06-06 17:38:06 +0530

    PersonalDetails file is updated by amar jsp2,wgl

--- src/com/istar/pojo/PersonalDetails.java
+++ src/com/istar/pojo/PersonalDetails.java
@@ -2,6 +2,36 @@ package com.istar.pojo;
 
 public class PersonalDetails
 {
-
-	
+private int id;
+private String name;
+private Long telno;
+private String dob;
+private String address;
+private String nickname;
+private String type;
+private String relationship;
+private String website;
+private String companyname;
+private Long landlineno;
+public PersonalDetails(){
+	super();
 }
+
+
+public PersonalDetails(int id,String name,Long telno,String dob, String address, String nickname,String type,String relationship,String website,String companyname,Long landlineno)
+{
+	super();
+	this.id=id;
+    this.name=name;
+    this.telno=telno;
+    this.dob=dob;
+    this.address=address;
+    this.nickname=nickname;
+    this.type=type;
+    this.relationship=relationship;
+    this.website=website;
+    this.companyname=companyname;
+    this.landlineno=landlineno;
+}
+
+
